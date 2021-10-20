5ea94b138d952d5bf5b50714ec016f771226092b
EwenBernard <73660111+EwenBernard@users.noreply.github.com>
2021-10-20 11:30:09 +0200

    Add init_date()

--- Project_Script.sql
+++ Project_Script.sql
@@ -1,11 +1,17 @@
+
+drop database database_project;
+create database if not exists database_project;
+use database_project;
+
 DROP TABLE IF EXISTS Theatre;
 CREATE TABLE Theatre(
         id_company   Int  Auto_increment,
-        id_theatre     Int (25),
-        capacity     Int (25),
+        -- id_theatre     Int Auto,
+        capacity     Int,
         budget     Float (25),
         city     Char (25),
-        PRIMARY KEY (id_company,id_theatre)
+        PRIMARY KEY (id_company)
+        -- PRIMARY KEY (id_company,id_theatre)
 )ENGINE=InnoDB;
 
 
@@ -29,7 +35,7 @@ CREATE TABLE Ticket(
         reduct_price     Bool,
         price     Float (25),
         date__Reserver     Date,
-        id_spectacle_Spectacle   int  Auto_increment,
+        id_spectacle_Spectacle   int,
         PRIMARY KEY (id_ticket)
 )ENGINE=InnoDB;
 
@@ -42,8 +48,8 @@ CREATE TABLE Sponsor(
         price_Subventionner     Float (25),
         date_start_Subventionner     Date,
         date_end_Subventionner     Date,
-        id_company_Theatre   int Auto_increment,
-        id_theatre_Theatre     Int (25),
+        id_company_Theatre   int,
+        -- id_theatre_Theatre     Int (25),
         PRIMARY KEY (id_sponsor)
 )ENGINE=InnoDB;
 
@@ -51,10 +57,11 @@ CREATE TABLE Sponsor(
 
 DROP TABLE IF EXISTS Produire;
 CREATE TABLE Produire(
-        id_company_Theatre   int Auto_increment,
-        id_theatre_Theatre     Int (25),
-        id_spectacle_Spectacle    int Auto_increment,
-        PRIMARY KEY (id_company_Theatre,id_theatre_Theatre,id_spectacle_Spectacle)
+        id_company_Theatre   int,
+        -- id_theatre_Theatre     Int (25),
+        id_spectacle_Spectacle    int,
+        -- PRIMARY KEY (id_company_Theatre,id_theatre_Theatre,id_spectacle_Spectacle)
+        PRIMARY KEY (id_company_Theatre, id_spectacle_Spectacle)
 )ENGINE=InnoDB;
 
 
@@ -66,40 +73,48 @@ CREATE TABLE Accueillie(
         date_end     Date,
         frais_transport     Float (25),
         frais_mes     Float (25),
-        id_company_Theatre    int Auto_increment,
-        id_theatre_Theatre     Int (25),
-        id_spectacle_Spectacle   int  Auto_increment,
-        PRIMARY KEY (id_company_Theatre,id_theatre_Theatre,id_spectacle_Spectacle)
+        id_company_Theatre    int,
+        -- id_theatre_Theatre     Int (25),
+        id_spectacle_Spectacle   int,
+        PRIMARY KEY (id_company_Theatre, id_spectacle_Spectacle)
+        -- PRIMARY KEY (id_company_Theatre,id_theatre_Theatre,id_spectacle_Spectacle)
 )ENGINE=InnoDB;
 
-CREATE TABLE CalendarMonths (
+CREATE TABLE Calendar (
   date DATETIME,
   PRIMARY KEY (date)
 )ENGINE=InnoDB;
 
-DELIMITER //
+DELIMITER /
 CREATE PROCEDURE init_date()
 	BEGIN
-		DECLARE basedate DATETIME;
-		DECLARE offset INT;
-	SELECT
-		basedate = '01 Jan 2000',
-		offset = 1;
+    
+	DECLARE basedate DATETIME;
+	DECLARE offset INT;
+	SET basedate = "2021-01-01";
+	SET	offset = 1;
 
-	WHILE (offset < 2048) DO
-		INSERT INTO CalendarMonths SELECT DATEADD(MONTH, @offset, date) FROM CalendarMonths;
-		SELECT offset = offset + offset;
+	WHILE (offset < 60) DO
+        INSERT INTO Calendar VALUES (basedate);
+        SET basedate = DATE_ADD(basedate, INTERVAL 1 DAY);
+        SET offset = offset + 1;
 	END WHILE;
-END //
+END /
 
 DELIMITER ;
 
 ALTER TABLE Ticket ADD CONSTRAINT FK_Ticket_id_spectacle_Spectacle FOREIGN KEY (id_spectacle_Spectacle) REFERENCES Spectacle(id_spectacle);
 ALTER TABLE Sponsor ADD CONSTRAINT FK_Sponsor_id_company_Theatre FOREIGN KEY (id_company_Theatre) REFERENCES Theatre(id_company);
-ALTER TABLE Sponsor ADD CONSTRAINT FK_Sponsor_id_theatre_Theatre FOREIGN KEY (id_theatre_Theatre) REFERENCES Theatre(id_theatre);
+-- ALTER TABLE Sponsor ADD CONSTRAINT FK_Sponsor_id_theatre_Theatre FOREIGN KEY (id_theatre_Theatre) REFERENCES Theatre(id_theatre);
 ALTER TABLE Produire ADD CONSTRAINT FK_Produire_id_company_Theatre FOREIGN KEY (id_company_Theatre) REFERENCES Theatre(id_company);
-ALTER TABLE Produire ADD CONSTRAINT FK_Produire_id_theatre_Theatre FOREIGN KEY (id_theatre_Theatre) REFERENCES Theatre(id_theatre);
+-- ALTER TABLE Produire ADD CONSTRAINT FK_Produire_id_theatre_Theatre FOREIGN KEY (id_theatre_Theatre) REFERENCES Theatre(id_theatre);
 ALTER TABLE Produire ADD CONSTRAINT FK_Produire_id_spectacle_Spectacle FOREIGN KEY (id_spectacle_Spectacle) REFERENCES Spectacle(id_spectacle);
 ALTER TABLE Accueillie ADD CONSTRAINT FK_Accueillie_id_company_Theatre FOREIGN KEY (id_company_Theatre) REFERENCES Theatre(id_company);
-ALTER TABLE Accueillie ADD CONSTRAINT FK_Accueillie_id_theatre_Theatre FOREIGN KEY (id_theatre_Theatre) REFERENCES Theatre(id_theatre);
+-- ALTER TABLE Accueillie ADD CONSTRAINT FK_Accueillie_id_theatre_Theatre FOREIGN KEY (id_theatre_Theatre) REFERENCES Theatre(id_theatre);
 ALTER TABLE Accueillie ADD CONSTRAINT FK_Accueillie_id_spectacle_Spectacle FOREIGN KEY (id_spectacle_Spectacle) REFERENCES Spectacle(id_spectacle);
+
+
+
+CALL init_date();
+
+select * from Calendar;
